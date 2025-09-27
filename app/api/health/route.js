a7169dc7790892dd718ed10ae2f912e39e7d07da
ShivamKureel1122 <sunnykureel1122@gmail.com>
2025-09-27 12:42:06 +0530

    updated health endpoint

--- app/api/health/route.js
+++ app/api/health/route.js
@@ -1,4 +1,13 @@
 
 export async function GET() {
-  return new Response("OK", { status: 200 });
+    const date = new Date();
+    const timeInIndia = date.toLocaleTimeString("en-IN", {
+        timeZone: "Asia/Kolkata",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: false // change to 24-hour format
+    });
+    console.log(`[HEALTH CHECK] Ping at ${timeInIndia}`);
+    return new Response("OK", { status: 200 });
 }
